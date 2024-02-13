2fadcfd5187ce1e27fd8a9439b64ff4d0849b6e4
ajimad <ajbar.imad.99@gmail.com>
2024-02-13 08:46:37 +0100

    Small modification check if the changes work properly

--- todo-list-ui/app/page.tsx
+++ todo-list-ui/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Todo list
+      Todo list app
     </main>
   );
 }
